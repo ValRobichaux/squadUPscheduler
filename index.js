ee5264f40dfa2fcc2f16223dc9448bd610b246b2
valrobichaux <val.robichaux@gmail.com>
2023-05-07 09:56:17 -0500

    added extremely basic functionality

---
+++ index.js
@@ -0,0 +1,24 @@
+require('dotenv/config')
+const { Client, GatewayIntentBits } = require('discord.js');
+
+
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+client.on('ready',() => {
+    console.log('The bot is ready')
+})
+client.on('messageCreate',message => {
+    if (message.content === 'ping') {
+        message.reply('pong')
+    }
+})
+client.login(process.env.DISCORD_TOKEN)
